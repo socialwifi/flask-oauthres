9219ebc17af36752814cb0406ec5160a427abc5c
Jakub Gocławski <jakub@goclawski.net>
2017-02-28 12:13:07 +0100

    Support Python 3.6.

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
